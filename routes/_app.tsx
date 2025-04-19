a7dee4dee04a71a5863321f0f0884b6c49fac5ac
LesserNalsa <lessernalsa@gmail.com>
2025-04-20 00:26:23 +0900

    chore: _app.tsx 및 css 설정 수정
    - _app.tsx를 앞으로 작업에 용이하도록 태그와 클래스 수정
    - deno.json에서 모듈 호출을 위한 모듈 주소 설정 추가
    - tailwind.config.ts에 custom color, 폰트 추가 및 모듈 추가가

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,18 @@
+/** @jsx h */
+import { h } from 'preact';
 import { type PageProps } from "$fresh/server.ts";
+
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="ko">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>lessernalsa-portfolio-publisher</title>
+        <title>LesserNalsa</title>
+        <link rel="icon" href="/images/favicon.ico" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body>
+      <body class="bg-white text-gray-800 font-sans">
         <Component />
       </body>
     </html>
--- styles/tailwind.config.ts
+++ styles/tailwind.config.ts
@@ -1,7 +1,24 @@
-import { type Config } from "tailwindcss";
+// styles/tailwind.config.ts
+import { Options } from "$fresh/plugins/twind.ts";
+import presetAutoprefix from "twind-preset-autoprefix";
+import presetTailwind from "twind-preset-tailwind";
 
 export default {
-  content: [
-    "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
-  ],
-} satisfies Config;
+  // Tailwind의 기본 설정을 불러오는 프리셋
+  presets: [presetTailwind(), presetAutoprefix()],
+  theme: {
+    extend: {
+      colors: {
+        lessernavy: "#2c3e50",
+        mint: "#98f5e1",
+        champagne: "#f7e8b6",
+        lightgold: "#f5deb3",
+        burgundy: "#7f1d1d",
+        sky: "#cfe8fc",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+} as unknown as Options;
